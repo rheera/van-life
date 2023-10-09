041c06b8b88a637de22cab5f69dbe905def15f0f
rheera <rheera2@uwo.ca>
2023-10-09 18:45:26 -0400

    set up van filters using search params

--- src/views/Vans/Vans.tsx
+++ src/views/Vans/Vans.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Van } from "../../types/interfaces";
 import { VanTypes } from "../../types/enums";
 import { vanTypeButtonColor } from "../../utils/functions";
@@ -8,6 +8,20 @@ import Button from "react-bootstrap/Button";
 
 const Vans = () => {
   const [vanData, setVanData] = useState<Van[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterType = searchParams.get("type");
+
+  const handleQueryChange = (key: string, value: string | null) => {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  };
 
   useEffect(() => {
     fetch("/api/vans")
@@ -15,16 +29,25 @@ const Vans = () => {
       .then((data) => setVanData(data.vans));
   }, []);
 
+  const filteredVans = filterType
+    ? vanData.filter(({ type }) => type === filterType)
+    : vanData;
+
   // Map through the enums with each type of Van, add types there when new types of vans added
   const displayVanFilters = Object.keys(VanTypes).map((type) => {
     return (
-      <Button key={`filter-${type}`} className="vans__filters__button">
+      <Button
+        onClick={() => handleQueryChange("type", type)}
+        key={`filter-${type}`}
+        className={`vans__filters__button vans__filters__button--${type} ${
+          filterType === type ? `vans__filters__button--${type}--selected` : ""
+        }`}
+      >
         {type}
       </Button>
     );
   });
-
-  const displayVanData = vanData.map((van) => (
+  const displayVanData = filteredVans.map((van) => (
     <div key={van.id} className="vans__all-vans__van">
       <Link to={`/vans/${van.id}`}>
         <div className="zoom-img">
@@ -40,7 +63,11 @@ const Vans = () => {
           </div>
         </div>
       </Link>
-      <Button variant={vanTypeButtonColor(van.type)} className="van-type-badge">
+      <Button
+        onClick={() => handleQueryChange("type", van.type)}
+        variant={vanTypeButtonColor(van.type)}
+        className="van-type-badge"
+      >
         {van.type}
       </Button>
     </div>
@@ -52,9 +79,14 @@ const Vans = () => {
         <h2>Explore our van options</h2>
         <div className="vans__filters">
           {displayVanFilters}
-          <button className="underline-link vans__filters__clear">
-            Clear filters
-          </button>
+          {filterType && (
+            <button
+              onClick={() => handleQueryChange("type", null)}
+              className="underline-link vans__filters__clear"
+            >
+              Clear filters
+            </button>
+          )}
         </div>
         {vanData.length > 0 ? (
           <div className="vans__all-vans">{displayVanData}</div>
