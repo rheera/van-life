7059b51be15442299d96bf923f4e2d2ffbaef3cd
rheera <rheera2@uwo.ca>
2023-10-22 19:58:00 -0400

    collapseOnSelect nav fix

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -4,31 +4,58 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { FaRegUserCircle } from "react-icons/fa";
 import logo from "../assets/images/van-logo.png";
+import { useState } from "react";
 
 const SiteNav = () => {
   const activeClass = ({ isActive }: { isActive: boolean }) => {
     return isActive ? "nav-link nav__link active-link" : "nav-link nav__link";
   };
+  // need to add this expanded code since collapseOnSelect doesn't work with NavLinks
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="sm" className="nav">
+    <Navbar expand="sm" className="nav" expanded={expanded}>
       <Container>
         <Navbar.Brand className="nav__brand" to="." as={Link}>
           <img src={logo} alt="Van Life Logo" />
           <h1>#VANLIFE</h1>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="site-navbar" />
+        <Navbar.Toggle
+          aria-controls="site-navbar"
+          onClick={() => setExpanded((prevState) => !prevState)}
+        />
         <Navbar.Collapse id="site-navbar" className="justify-content-end">
           <Nav>
-            <NavLink to="host" className={activeClass} role="button">
+            <NavLink
+              to="host"
+              onClick={() => setExpanded(false)}
+              className={activeClass}
+              role="button"
+            >
               Host
             </NavLink>
-            <NavLink className={activeClass} to="vans" role="button">
+            <NavLink
+              onClick={() => setExpanded(false)}
+              className={activeClass}
+              to="vans"
+              role="button"
+            >
               Vans
             </NavLink>
-            <NavLink className={activeClass} to="about" role="button">
+            <NavLink
+              onClick={() => setExpanded(false)}
+              className={activeClass}
+              to="about"
+              role="button"
+            >
               About
             </NavLink>
-            <NavLink className={activeClass} to="login" role="button">
+            <NavLink
+              onClick={() => setExpanded(false)}
+              className={activeClass}
+              to="login"
+              role="button"
+            >
               <FaRegUserCircle />
             </NavLink>
           </Nav>
