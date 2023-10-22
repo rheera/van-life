5fa0d1e02b04a67707dc703b096ef585852f5869
rheera <rheera2@uwo.ca>
2023-10-22 15:57:29 -0400

    add defer and Await to all fetching pages

--- src/views/Host/HostVans.tsx
+++ src/views/Host/HostVans.tsx
@@ -1,21 +1,26 @@
 import "../../scss/host-vans.scss";
 import ListVans from "../../components/ListVans";
 import { getHostVans } from "../../api/api";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, defer, Await } from "react-router-dom";
+import { Suspense } from "react";
 import { Van } from "../../types/interfaces";
 import { requireAuth } from "../../utils/functions";
 
 export const loader = async ({ request }: { request: Request }) => {
   await requireAuth(request);
-  return getHostVans("123");
+  return defer({ vans: getHostVans("123") });
 };
 
 const HostVans = () => {
-  const vanData = useLoaderData() as Van[];
+  const vanData = useLoaderData();
   return (
     <section className="host-vans">
       <h2>Your listed vans</h2>
-      <ListVans amtOfVansToShow={10} vans={vanData} />
+      <Suspense fallback={<h2>Loading your vans...</h2>}>
+        <Await resolve={(vanData as { vans: Van[] }).vans}>
+          <ListVans amtOfVansToShow={10} />
+        </Await>
+      </Suspense>
     </section>
   );
 };
